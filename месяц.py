8f6ebb56d1801d155b1fa7d722dda93b5f4ca7f7
about_bot <kunichkin83@gmail.com>
2024-01-22 00:44:06 +0300

    22 января(1)

--- месяц.py
+++ месяц.py
@@ -4,10 +4,11 @@ from datetime import datetime, timedelta
 AREA = 1  # Код региона (Москва)
 PERIOD = 30  # Период до сегодняшнего дня за который опубликована вакансия
 
+
 def get_vacancies_hh(language):
     url = 'https://api.hh.ru/vacancies'
     per_page = 100  # Количество вакансий на одной странице
-    vacancies_processed = 0
+    vacancies_info = []
 
     today = datetime.now().date()
     period_start = today - timedelta(days=PERIOD)
@@ -17,7 +18,7 @@ def get_vacancies_hh(language):
         params = {
             'text': language,
             'area': AREA,
-            'date_from': period_start.isoformat(),  # Устанавливаем начальную дату периода
+            'date_from': period_start.isoformat(),
             'per_page': per_page,
             'page': page
         }
@@ -26,34 +27,24 @@ def get_vacancies_hh(language):
 
         page_payload = response.json()
 
-        for i in range(len(page_payload["items"])):
-            vacancy = page_payload["items"][i]
-            print(f"Информация о вакансии {i + 1}:")
-            print("Название вакансии:", vacancy["name"])
-            print("URL вакансии:", vacancy["alternate_url"])
-
+        for vacancy in page_payload["items"]:
             if "employer" in vacancy and "url" in vacancy["employer"]:
                 url_employer = vacancy["employer"]["url"]
-                print("Выполняется запрос к сайту работодателя...")
                 response_employer = requests.get(url_employer)
-                response_employer.raise_for_status()
-                print("Запрос к сайту работодателя выполнен успешно.")
-
-                employer_info = response_employer.json()
-                print("Название компании:", employer_info["name"])
-                print("URL сайта компании:", employer_info["site_url"])
-            else:
-                print("Информация о работодателе отсутствует.")
-
-            print("---")
-
-            vacancies_processed += 1
+                if response_employer.status_code == 200:
+                    employer_info = response_employer.json()
+                    company_data = {
+                        "Компания": employer_info["name"],
+                        "Сайт": employer_info.get("site_url", "Информация отсутствует")
+                    }
+                    vacancies_info.append(company_data)
 
         if page_payload["page"] < page_payload["pages"] - 1:
             page += 1
         else:
             break
 
-    print(f"Обработано всего вакансий: {vacancies_processed}")
+    return vacancies_info
+
 
 get_vacancies_hh("Водитель")
