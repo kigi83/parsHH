ae7f39fde6b372cb4de4c23dafae588f3208de77
about_bot <kunichkin83@gmail.com>
2024-01-21 14:13:44 +0300

    21 января(2)

--- месяц.py
+++ месяц.py
@@ -4,7 +4,6 @@ from datetime import datetime, timedelta
 AREA = 1  # Код региона (Москва)
 PERIOD = 30  # Период до сегодняшнего дня за который опубликована вакансия
 
-
 def get_vacancies_hh(language):
     url = 'https://api.hh.ru/vacancies'
     per_page = 100  # Количество вакансий на одной странице
@@ -27,14 +26,30 @@ def get_vacancies_hh(language):
 
         page_payload = response.json()
 
-        for vacancy in page_payload["items"]:
+        for i in range(len(page_payload["items"])):
+            vacancy = page_payload["items"][i]
+            print(f"Информация о вакансии {i + 1}:")
             print("Название вакансии:", vacancy["name"])
             print("URL вакансии:", vacancy["alternate_url"])
-            # Другая обработка данных о вакансиях
+
+            if "employer" in vacancy and "url" in vacancy["employer"]:
+                url_employer = vacancy["employer"]["url"]
+                print("Выполняется запрос к сайту работодателя...")
+                response_employer = requests.get(url_employer)
+                response_employer.raise_for_status()
+                print("Запрос к сайту работодателя выполнен успешно.")
+
+                employer_info = response_employer.json()
+                print("Название компании:", employer_info["name"])
+                print("URL сайта компании:", employer_info["site_url"])
+            else:
+                print("Информация о работодателе отсутствует.")
+
+            print("---")
 
             vacancies_processed += 1
 
-        if page_payload["pages"] > page + 1:
+        if page_payload["page"] < page_payload["pages"] - 1:
             page += 1
         else:
             break
